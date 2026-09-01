9813f0adb7d943277899e2379f5f9249899a0762
EliteTQ <jonartbaj@gmail.com>
2026-08-31 20:24:22 -0600

    .

--- hid-controller-code/hid-controller-code/USB_DEVICE/App/usbd_desc.c
+++ hid-controller-code/hid-controller-code/USB_DEVICE/App/usbd_desc.c
@@ -62,10 +62,10 @@
   * @{
   */
 
-#define USBD_VID     0x45E
+#define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "Jonart B"
-#define USBD_PID_FS     0x28E
+#define USBD_PID_FS     22355
 #define USBD_PRODUCT_STRING_FS     "J-Controller"
 #define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
 #define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
